b985fe7e0bcfa24d6b8a8c4f9dbecfdd489d712e
asi7296 <ansujanani@hotmail.com>
2017-05-20 20:55:08 +0530

    made changed

--- number_base_convert/basecnvlib.c
+++ number_base_convert/basecnvlib.c
@@ -19,6 +19,7 @@ struct args* convBase(struct args *arg_struct) {
 	return arg_struct;
 }
 
+
 // decimal
 struct args* decConv(struct args *arg_struct) {
 	
@@ -44,58 +45,56 @@ struct args* decConv(struct args *arg_struct) {
 		arg_struct->num_out = reverseString(arg_struct->num_out);
 		return arg_struct;
 	}
-	
-	// to hexadecimal
-	else if(!strcmp(arg_struct->bout, "hexadecimal")) {
+
+	else if(!strcmp(arg_struct->bout, "octal")) {
 		int t = atoi( arg_struct->num_in );
-		
+		char *t_cc;
 		while( t != 0 ) {
-			arg_struct->num_out = (char *) realloc(arg_struct->num_out, sizeof(arg_struct->num_out) + 1);	
-			// + 48 -> put into ascii range
-			// char -> char * for strcat
-			char t_c = t % 16;
-
-			if( t_c <= 9 ) {
-				t_c += 48;
-				t_c = (char) t_c;
-			}
-			else {
-				switch(t_c) {
-					case 10: t_c = 'A'; break;
-					case 11: t_c = 'B'; break;
-					case 12: t_c = 'C'; break;
-					case 13: t_c = 'D'; break;
-					case 14: t_c = 'E'; break;
-					case 15: t_c = 'F'; break;
-				}
-			}
-
-			// ################ BUG HERE ################
-			char *t_cc = &t_c;
-			printf("%s\n", t_cc);
-			// ##########################################
-
-			strcat(arg_struct->num_out,  t_cc );
-			t /= 16;
-			//printf("\n***t_c: %c t_cc: %s num_out: %s***\n", t_c, t_cc, arg_struct->num_out);
+			arg_struct->num_out = (char *) realloc(arg_struct->num_out, sizeof(arg_struct->num_out) + 1);
+			t_cc = (char *) realloc(t_cc, sizeof(arg_struct->num_out));	
+			int t_c = t % 8;
+			sprintf(t_cc, "%d", t_c);
+			strcat(arg_struct->num_out, t_cc);
+			t /= 8;
 		}
+		//free(t_cc);
 		// strrev does not exist in linux string.h
 		arg_struct->num_out = reverseString(arg_struct->num_out);
 		return arg_struct;
 	}
-	
 
-	// to octal
-	else if(!strcmp(arg_struct->bout, "octal")) {
+
+	// to hexadecimal
+	else if(!strcmp(arg_struct->bout, "hexadecimal")) {
 		int t = atoi( arg_struct->num_in );
+		char *t_cc;
 		while( t != 0 ) {
-			arg_struct->num_out = (char *) realloc(arg_struct->num_out, sizeof(arg_struct->num_out) + 1);	
-			// + 48 -> put into ascii range
-			// char -> char * for strcat
-			char t_c = (char) t % 8 + 48;
-			char *t_cc = &t_c;
-			strcat(arg_struct->num_out,  t_cc );
-			t /= 8;
+			arg_struct->num_out = (char *) realloc(arg_struct->num_out, sizeof(arg_struct->num_out) + 1);
+			
+			// ### Why is this giving a memory error?
+			t_cc = (char *) realloc(t_cc, sizeof(arg_struct->num_out));	
+			// ######################################
+			
+			int t_c = t % 16;
+			printf("%d\n", t_c);
+			if( t_c <= 9 ) {
+				sprintf(t_cc, "%d", t_c);
+			}
+			else {
+				char temp_h;
+				switch(t_c) {
+					case 10: temp_h = 'A'; break;
+					case 11: temp_h = 'B'; break;
+					case 12: temp_h = 'C'; break;
+					case 13: temp_h = 'D'; break;
+					case 14: temp_h = 'E'; break;
+					case 15: temp_h = 'F'; break;
+				}
+				sprintf(t_cc, "%d", sprintf(t_cc, "%d", t_c));
+			}
+
+			strcat(arg_struct->num_out,  t_cc);
+			t /= 16;
 		}
 		// strrev does not exist in linux string.h
 		arg_struct->num_out = reverseString(arg_struct->num_out);
@@ -108,21 +107,21 @@ struct args* decConv(struct args *arg_struct) {
 
 // binary - convert to decimal
 struct args* binConv(struct args *arg_struct) {
-	
+	// convert to decimal for input value num_in, then call decConv which will take decimal and convert
 	return arg_struct;
 }
 
 
 // hexadecimal - convert to decimal
 struct args* hexadecConv(struct args *arg_struct) {
-	
+	// convert to hexadecimal for input value num_in, then call decConv which will take decimal and convert
 	return arg_struct;
 }
 
 
 // octal - convert to decimal
 struct args* octConv(struct args *arg_struct) {
-	
+	// convert to octal for input value num_in, then call decConv which will take decimal and convert
 	return arg_struct;
 }
 
